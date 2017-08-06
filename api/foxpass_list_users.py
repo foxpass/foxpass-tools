d652879d47a9276e5d005341d8ba40bf675749ff
Travis Theune <travist@delmarvelo.us>
2017-08-06 17:29:58 +0200

    Api tools (#2)
    * Some sample API scripts

---
+++ api/foxpass_list_users.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+This script requires the external libraries from requests
+pip install requests
+
+To run:
+python foxpass_list_users.py --api-key <api_key>
+"""
+import argparse
+import requests
+
+URL = 'https://api.foxpass.com/v1/'
+ENDPOINT = 'users/'
+
+def main():
+    parser = argparse.ArgumentParser(description='List groups in Foxpass')
+    parser.add_argument('--api-key', required=True, help='Foxpass API Key')
+    args = parser.parse_args()
+
+    header = {'Authorization': 'Token ' + args.api_key}
+    r = requests.get(URL + ENDPOINT, headers=header)
+    output(r)
+
+def output(r):
+    data = r.json()['data']
+    for group in data:
+        print group['username']
+
+if __name__ == '__main__':
+    main()
