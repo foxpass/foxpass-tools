ed99380805e433e2939aee3bcd1c4e69c577b60b
Travis Theune <travist@tenminutesout.net>
2017-09-12 17:49:24 +0200

    Tweak logging output for ssh.

--- testing/setup-test.py
+++ testing/setup-test.py
@@ -165,7 +165,7 @@ def ssh(ip, user, command, verbose=False, once=False):
             if verbose:
                 return output
             else:
-                LOGGER.warning('Failed to run: %s', command)
+                LOGGER.warning('Failed to run: ssh %s -l %s -t "%s"', ip, user, command)
                 return
         if once:
             if verbose:
