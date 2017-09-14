1c6a06af690a32d4a628a665ffdd8f55ec1e0e14
Travis Theune <travist@tenminutesout.net>
2017-09-14 13:33:53 +0200

    component_list prints out data now

--- audit/component_list.py
+++ audit/component_list.py
@@ -4,6 +4,17 @@
 
 import boto.ec2
 import boto.rds
+import pprint
+
+def main():
+    for region in regions():
+        print region
+        i_info = instance_info(region)
+        d_info = db_info(region)
+        if i_info:
+            pprint.pprint(i_info)
+        if d_info:
+            pprint.pprint(d_info)
 
 def regions():
     regions = boto.ec2.connect_to_region('us-east-1').get_all_regions()
@@ -13,13 +24,21 @@ def regions():
 def instance_info(region):
     ec2 = boto.ec2.connect_to_region(region)
     info = {}
-    instances = ec2.get_only_instances()
+    instances = ec2.get_only_instances(filters={'instance-state-name': 'running'})
     for instance in instances:
-        info[instance.id] = {
-                                     'region': region,
-                                     'ami_id': instance.image_id,
-                                     'ami_desc': ec2.get_all_images(instance.image_id)[0].description
-                                     }
+        info[instance.id] = {'region': region, 'ami_id': instance.image_id}
+        ami_desc = ec2.get_all_images(instance.image_id)
+        if ami_desc:
+            info[instance.id]['ami_desc'] = ami_desc[0].description
     return info
 
 def db_info(region):
+    rds = boto.rds.connect_to_region(region)
+    info = {}
+    instances = rds.get_all_dbinstances()
+    for instance in instances:
+        info[instance.id] = {'region': region, 'engine': instance.engine}
+    return info
+
+if __name__ == '__main__':
+    main()
