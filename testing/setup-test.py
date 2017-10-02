496c97f7617cacc69610c9e5ee169971448c1482
Travis Theune <travist@delmarvelo.us>
2017-10-02 19:50:43 +0200

    Multiprocess (#4)
    * First pass at multiprocess
    
    * Moved ec2 connection and instance creation into
    individual processes.
    Shrunk AMIS to single instance for testing
    Passed ec2 object into various functions since it is
    not a global anymore.

--- testing/setup-test.py
+++ testing/setup-test.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-# aws-vault exec fp -- ./setup-test.py --ssh-key <key_name> --branch <test_branch> --bind-pw <bind_password> --api-key <foxpass_api_key>
+# aws-vault exec fp -- ./setup-test.py --ssh-key <key_name> --branch <test_branch> --bind-pw <bind_password> --api-key <foxpass_api_key> [--ami-name <AMI NAME>]
 
-from __future__ import print_function
 from commands import getstatusoutput
+from multiprocessing import Pool
 import argparse
+import logging
+import multiprocessing
 import os
 import re
 import sys
@@ -28,16 +30,17 @@ IT = 't2.nano'              # save $$$
 SUB = 'subnet-4ec61216'     # This puts it in the testing VPC in us-west-2
 REGION_NAME = 'us-west-2'   # if you change this you must change the AMIs above
 USER = os.environ['USER']
-
-# Connect to us-west-2
-EC2 = boto.ec2.connect_to_region(REGION_NAME)
+LOGGER = multiprocessing.log_to_stderr()
+LOGGER.setLevel(logging.INFO)
+LOGGER.info('Starting test(s)')
 
 def main():
     parser = argparse.ArgumentParser(description='Run Foxpass setup script tests.')
-    parser.add_argument('--ssh-key', help='SSH Key name to use when launching instance.')
-    parser.add_argument('--branch', help='Github branch name to use for testing.')
-    parser.add_argument('--bind-pw', help='Foxpass ldap bind password')
-    parser.add_argument('--api-key', help='Foxpass API KEY for account connection.')
+    parser.add_argument('--ssh-key', required=True, help='SSH Key name to use when launching instance.')
+    parser.add_argument('--branch', required=True, help='Github branch name to use for testing.')
+    parser.add_argument('--bind-pw', required=True, help='Foxpass ldap bind password')
+    parser.add_argument('--api-key', required=True, help='Foxpass API KEY for account connection.')
+    parser.add_argument('--ami-name', default=False, help='Distro name to run as a single instance')
     args = parser.parse_args()
 
     # Non-global vaiables for launching instances and running the config script
@@ -45,40 +48,63 @@ def main():
     branch = args.branch
     bind_pw = args.bind_pw
     api_key = args.api_key
-    # Store the instance data here after launching
-    instances = {}
 
-    # Launch all of the instanes and store the name and instance id in instances{}
-    for name, ami in AMIS.items():
-        print('Launching', name)
-        instances[name] = EC2.run_instances(ami, instance_type=IT, subnet_id=SUB, security_group_ids=[SG], key_name=key).instances[0].id
+    # Check to see if we are running just one ami or the whole list
+    if args.ami_name:
+        name, ami = ami_check(args.ami_name)
+        test_instance(key, branch, bind_pw, api_key, name, ami)
+    else:
+        multi_process(key, branch, bind_pw, api_key)
 
-    # Connect to each instance and run the appropriate script for Foxpass
-    for name, id in instances.items():
-        # Get instance state and wait for it to be running
-        instance_wait(id, name)
-        # Build the command to run per instance
-        command, ip = build_command(id, name, branch, bind_pw, api_key)
-        # Run the command on each instance
-        run_command(name, ip, command)
-        # Test the setup!
-        test_result(ip, name)
+# Check ami_name
+def ami_check(name):
+    if name in AMIS.keys():
+        return name, AMIS[name]
+    else:
+        LOGGER.warning('Do not know how to configure this distro, please update setup-test.py with parameters for %s.', name)
+        exit()
+
+# Run all every AMI we test
+def multi_process(key, branch, bind_pw, api_key):
+    # Create a process pool large enough to run all of the instances in parallel
+    pool = Pool(len(AMIS))
+    # Build an array with all of the parameters that get called in test_instance()
+    tests = [(key, branch, bind_pw, api_key, name, ami) for name, ami in AMIS.items()]
+    # Pass the parameters for each process call into star_test_instance() to expand the arguments
+    for run in pool.map(star_test_instance, tests):
+        r = run     # Run each process
+
+# Expand the parameters passed in by pool.map() to test_instance()
+def star_test_instance(args):
+    return test_instance(*args)
+
+# Connect to instance and run the appropriate script for Foxpass
+def test_instance(key, branch, bind_pw, api_key, name, ami):
+    # Connect to EC2
+    ec2 = boto.ec2.connect_to_region(REGION_NAME)
+    # Launch an instance and save the instance_id
+    LOGGER.info('Launching %s.', name)
+    instance_id = ec2.run_instances(ami, instance_type=IT, subnet_id=SUB, security_group_ids=[SG], key_name=key).instances[0].id
+    # Get instance state and wait for it to be running
+    instance_wait(ec2, instance_id, name)
+    # Build the command to run per instance
+    command, ip = build_command(ec2, instance_id, name, branch, bind_pw, api_key)
+    # Run the command on each instance
+    run_command(name, ip, command)
+    # Test the setup!
+    test_result(ip, name)
 
 # Let each instance finish booting before trying to connect
-def instance_wait(id, name):
-    print('Waiting for', name, 'to launch.', end='')
-    sys.stdout.flush()
-    status = EC2.get_only_instances(id)[0].state
+def instance_wait(ec2, instance_id, name):
+    LOGGER.info('Waiting for %s to boot.', name)
+    status = ec2.get_only_instances(instance_id)[0].state
     while status != 'running':
         time.sleep(5)
-        status = EC2.get_only_instances(id)[0].state
-        print('.', end='')
-        sys.stdout.flush()
-    print('', end='\n')
+        status = ec2.get_only_instances(instance_id)[0].state
 
 # Create custom command based on instance data
-def build_command(id, name, branch, bind_pw, api_key):
-    ip = EC2.get_only_instances(id)[0].ip_address
+def build_command(ec2, instance_id, name, branch, bind_pw, api_key):
+    ip = ec2.get_only_instances(instance_id)[0].ip_address
     dist = re.search('^\w+', name).group(0)
     ver = re.search('\d+.*', name).group(0)
     url = 'https://raw.githubusercontent.com/foxpass/foxpass-setup/%s/linux/%s/%s/foxpass_setup.py' % (branch, dist, ver)
@@ -90,8 +116,7 @@ def build_command(id, name, branch, bind_pw, api_key):
              '--ldap-uri', 'ldaps://foxfood.foxpass.com',
              '--api-url', 'https://foxfood.foxpass.com/api', '2>/dev/null']
     command = 'wget %s 2>/dev/null && chmod 755 foxpass_setup.py && sudo ./%s' % (url, ' '.join(setup))
-    print('Configuring', name)
-    sys.stdout.flush()
+    LOGGER.info('Configuring %s.', name)
     return command, ip
 
 def run_command(name, ip, command):
@@ -101,7 +126,7 @@ def run_command(name, ip, command):
         # wget is not installed by default, so prepend that to command
         command = 'sudo yum install -y wget 2>/dev/null &&' + command
         ssh(ip, 'centos', command)    # configure the remote host
-        ssh(ip, USER, 'ls', fail=True) # need to have selinux block a curl command from foxpass_ssh_keys.sh
+        ssh(ip, USER, 'ls', once=True) # need to have selinux block a curl command from foxpass_ssh_keys.sh
         # Now you can adjust selinux
         ssh(ip, 'centos', "sudo ausearch -c 'curl' --raw | audit2allow -M my-curl && sudo semodule -i my-curl.pp")
     elif name == 'centos-6.5':
@@ -114,41 +139,45 @@ def run_command(name, ip, command):
     elif 'ubuntu' in name:
         ssh(ip, 'ubuntu', command)
     else:
-        print('Do not know how to configure this distro, please update setup-test.py with parameters for', name)
+        LOGGER.warning('Do not know how to configure this distro, please update setup-test.py with parameters for %s.', name)
 
 # Check to see if ldap logins and sudo work
 def test_result(ip, name):
-    result = re.search('root', ssh(ip, USER, 'sudo whoami', verbose=True, fail=True))
+    result = re.search('root', ssh(ip, USER, 'sudo whoami', verbose=True, once=True))
     if not result:
-        print(name, 'failed, log into', ip, 'and investigate.')
-        sys.stdout.flush()
+        LOGGER.warning('%s failed, log in and investigate.', name)
     else:
-        print(name, 'passed!')
-        sys.stdout.flush()
+        LOGGER.info('%s passed!', name)
 
 # Run a command on a remote host
-def ssh(ip, user, command, verbose=False, fail=False):
+def ssh(ip, user, command, verbose=False, once=False):
     # ip = target ip address
     # user = ssh user
     # command = remote command to run
     # verbose = return the results of ssh
-    # fail = if True, only try once
+    # once = if True, only try once
     count = 0
     while True:
         # Attempt to run the command, capture status and output from os command
         status, output = getstatusoutput('ssh %s -l %s -t "%s"' % (ip, user, command))
         # If successful or we are only trying once, exit the loop
-        if status == 0 or fail:
+        if status == 0:
+            if verbose:
+                return output
+            else:
+                return
+        else:
+            LOGGER.warning('Failed to run: ssh %s -l %s -t "%s"', ip, user, command)
+        if once:
             if verbose:
                 return output
             else:
                 return
         count += 1
         if count > 5:
-            print('SSH failed for', ip, 'after 5 attempts, investigate')
-        print('.', end='')
-        sys.stdout.flush()
-        time.sleep(5)
+            LOGGER.warning('SSH failed for %s after 5 attempts, investigate', ip)
+            return
+        time.sleep(10)
         pass
 
 if __name__ == '__main__':
