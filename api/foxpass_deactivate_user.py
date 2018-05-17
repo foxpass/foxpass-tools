d04ae74643e3201053a91fbd3070b2806e51e884
Travis Theune <travist@tenminutesout.net>
2018-05-17 14:38:52 +0200

    Make changes to comply with PEP8

--- api/foxpass_deactivate_user.py
+++ api/foxpass_deactivate_user.py
@@ -9,21 +9,24 @@ python foxpass_deactivate_user.py --api-key <api_key> --user <username>
 """
 import argparse
 import json
+
 import requests
+
 
 URL = 'https://api.foxpass.com/v1/'
 ENDPOINT = 'users/'
 DATA = {'is_active': False}
+
 
 def main():
     parser = argparse.ArgumentParser(description='Deactivate user in Foxpass')
     parser.add_argument('--api-key', required=True, help='Foxpass API Key')
     parser.add_argument('--user', required=True, help='Foxpass username')
     args = parser.parse_args()
-
     header = {'Authorization': 'Token ' + args.api_key}
     r = requests.put(URL + ENDPOINT + args.user + '/', headers=header, data=json.dumps(DATA))
     print r.json()
+
 
 if __name__ == '__main__':
     main()
