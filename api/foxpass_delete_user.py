07af0935124a4227cf53ac3482b9e8a08fda6fac
Travis Theune <travist@tenminutesout.net>
2018-05-17 21:29:38 +0200

    Create delete_user tool

---
+++ api/foxpass_delete_user.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+This script requires the external libraries from requests
+pip install requests
+
+To run:
+python foxpass_delete_user.py --api-key <api_key> --user <user name>
+"""
+
+import argparse
+
+import requests
+
+URL = 'https://api.foxpass.com/v1/'
+ENDPOINT = 'username/'
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Delete groups in Foxpass')
+    parser.add_argument('--api-key', required=True, help='Foxpass API Key')
+    parser.add_argument('--group-name', required=True, help='Foxpass group name')
+    args = parser.parse_args()
+    header = {'Authorization': 'Token ' + args.api_key}
+    r = requests.delete(URL + ENDPOINT + args.user + '/', headers=header)
+    print r.json()
+
+
+if __name__ == '__main__':
+    main()
