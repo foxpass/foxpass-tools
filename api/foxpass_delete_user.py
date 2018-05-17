50e08be90a910141f14515a996b7b01fe1d23ded
Travis Theune <travist@tenminutesout.net>
2018-05-17 23:20:13 +0200

    Fix naming in argparse help

--- api/foxpass_delete_user.py
+++ api/foxpass_delete_user.py
@@ -19,7 +19,7 @@ ENDPOINT = 'users/'
 def main():
     parser = argparse.ArgumentParser(description='Delete groups in Foxpass')
     parser.add_argument('--api-key', required=True, help='Foxpass API Key')
-    parser.add_argument('--username', required=True, help='Foxpass group name')
+    parser.add_argument('--username', required=True, help='Foxpass user name')
     args = parser.parse_args()
     header = {'Authorization': 'Token ' + args.api_key}
     r = requests.delete(URL + ENDPOINT + args.user + '/', headers=header)
